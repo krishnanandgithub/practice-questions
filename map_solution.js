3e31988ddae4bc15392f629f9863d04b3cffd23f
Krishnanand <110751379+krishnanandgithub@users.noreply.github.com>
2024-12-21 22:51:41 +0530

    added lengthOfStrings and upperCaseOfString

--- map_solution.js
+++ map_solution.js
@@ -11,3 +11,30 @@ const squaresOf = function (numbers) {
 const numbers = [1, 2, 3, 4, 5, 6];
 
 console.log(squaresOf(numbers));
+
+//---------------------------Q2---------------------
+
+const lengthOfString = function (string) {
+  return string.length;
+};
+
+const lengthsOf = function (strings) {
+  return strings.map(lengthOfString);
+};
+
+const strings = ["apple", "banana", "kiwi"];
+
+console.log(lengthsOf(strings));
+
+//---------------------------Q3---------------------
+
+const upperCase = function (string) {
+  return string.toUpperCase();
+};
+
+const uppercaseOf = function (strings) {
+  return strings.map(upperCase);
+};
+
+console.log(uppercaseOf(strings));
+console.log(uppercaseOf(["hello", "world"]));
