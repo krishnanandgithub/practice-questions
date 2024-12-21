7f89a659ab1608c579e3188232347693c8a2c3f0
Krishnanand <110751379+krishnanandgithub@users.noreply.github.com>
2024-12-21 16:46:01 +0530

    add filterLongestWords

--- filter_solution.js
+++ filter_solution.js
@@ -16,3 +16,21 @@ const evenNumbers3 = [0, 2, 4, 6, 8];
 console.log(filterEvenNumbers(evenNumbers1));
 console.log(filterEvenNumbers(evenNumbers2));
 console.log(filterEvenNumbers(evenNumbers3));
+
+//--------------------------------------Q2-----------------------------
+
+const lengthMoreThan = function (limit) {
+  return function (string) {
+    return string.length > limit;
+  };
+};
+
+const filterLongWords = function (words) {
+  return words.filter(lengthMoreThan(5));
+};
+
+const words1 = ["apple", "banana", "kiwi", "grape"];
+const words2 = ["ram", "shyam", "ramn", "ramesh", "rohit anna"];
+
+console.log(filterLongWords(words1));
+console.log(filterLongWords(words2));
